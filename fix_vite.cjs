332010e97dbefc7694d258bb681655b010847ea3
edibrata <edibratabanten@gmail.com>
2026-09-18 05:52:22 +0700

    Push

--- fix_vite.cjs
+++ fix_vite.cjs
@@ -1,13 +1,4 @@
 const fs = require('fs');
 let code = fs.readFileSync('vite.config.ts', 'utf-8');
-
-// For GitHub Pages, the base should usually match the repository name if it's a project site.
-// If the repo is LKBB-Yel-Yel, the base should be /LKBB-Yel-Yel/.
-// Alternatively, setting base to '' or './' works, but let's change it to match the repo name to be perfectly safe for Vite + GH Pages.
-
-code = code.replace(
-  /base: '\.\/',/,
-  `base: '/LKBB-Yel-Yel/',`
-);
-
+code = code.replace(/base: '\/LKBB-Yel-Yel\/',/, `base: './',`);
 fs.writeFileSync('vite.config.ts', code);
